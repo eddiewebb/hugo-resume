46ab98b7c3208699e2c789e97514b5065f4e29f1
ZeinShehab <zeinshehab@outlook.com>
2025-02-12 10:43:21 +0200

    Changed accent color to blue

---
+++ exampleSite/public/js/resume.js
@@ -0,0 +1,18 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+})(jQuery); // End of use strict
